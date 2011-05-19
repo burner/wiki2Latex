ef5d731f90396c324cf83ad9caa15ce074ec6584
Robert BuRnEr Schadek <rburners@gmail.com>
2011-05-19 11:46:46 +0200

    some more

--- rip.py
+++ rip.py
@@ -3,6 +3,7 @@ import urllib.request, urllib.error
 import re
 
 wikibook = ["http://en.wikibooks.org/w/index.php?title=","&action=edit"]
+wikipedia = ["http://en.wikipedia.org/w/index.php?title=","&action=edit"]
 
 def download(url, page):
 	save = []
@@ -16,7 +17,7 @@ def download(url, page):
 	good = False
 
 	for line in s:
-		jine = line.decode("utf-8")
+		jine = line.decode('utf-8')
 		it = jine.find("name=\"wpTextbox1\">")
 		if it != -1:
 			save.append(jine[it+len("name=\"wpTextbox1\">"):])
@@ -87,25 +88,61 @@ def getImages(line):
 		print(r)
 		print(len(line), it, jt)
 		ret.append(r)
-		it = line[it+len("[[Image:"):].find("[[Image:")
+		it = line[jt+len("]]"):].find("[[Image:")
+
+	return ret
+
+def checkIfPureLink(line):
+	it = line.find(":")
+	if it != -1:
+		return None
+	else:
+		jt = line.find("|")
+		if jt == -1:
+			return line
+		else:
+			return line[:jt]
+
+def getLinks(line):
+	ret = []
+	it = 0
+	it = line[it:].find("[[")
+	while it != -1: 
+		jt = line[it+len("[["):].find("]]") + it + len("[[")
+		print(it,jt,line[it+len("[["):jt])
+		#print(line)
+		s = makeUnderScore(line[it+len("[["):jt])
+		r = checkIfPureLink(s)
+		if r is not None:
+			print(s, r)
+			ret.append(r)
+		it = line[jt+len("]]"):].find("[[") 
+		if it == -1:
+			break
+		it += jt + len("]]")
 
 	return ret
 
 
 if __name__ == "__main__":
-	name = "LaTeX/Tables"
-	s = download(wikibook, name)
+	#name = "LaTeX/Tables"
+	name = "Data_structure"
+	s = download(wikipedia, name)
 
 	images = []
+	links = []
 	
 	f = open(makeName(name), "w")
 	cnt = 0
 	for i in s:
 		f.write(replaceHtml(i))
-		for j in getImages(i):
-			images.append(j)
+		#for j in getImages(i):
+		#	images.append(j)
+		for j in getLinks(i):
+			links.append(j)
 		cnt+=1
 
 	f.close()
 
 	#print(images)
+	print(links)
